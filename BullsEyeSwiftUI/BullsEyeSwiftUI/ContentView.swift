d62b0bb52126074797b4f3e304e96024d2437165
Tina Skordaki <tinaskordaki@gmail.com>
2024-02-23 18:19:46 +0300

    created project

---
+++ BullsEyeSwiftUI/BullsEyeSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BullsEyeSwiftUI
+//
+//  Created by Tina  on 23.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
