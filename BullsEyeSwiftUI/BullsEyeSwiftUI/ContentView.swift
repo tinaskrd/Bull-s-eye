06fd4ebd659c456195ebd862e96be7f4236ba6c0
Tina Skordaki <tinaskordaki@gmail.com>
2024-02-23 20:25:59 +0300

    made initial interface

--- BullsEyeSwiftUI/BullsEyeSwiftUI/ContentView.swift
+++ BullsEyeSwiftUI/BullsEyeSwiftUI/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAlert = false
+    @State private var point: Double = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center, spacing: 50) {
+            HStack{
+                Label("Put the Bull's Eye as close as you can to:", systemImage: "")
+                    .labelStyle(.titleOnly)
+                Label("100", systemImage: "")
+                    .labelStyle(.titleOnly)
+            }
+            HStack {
+                Label("1", systemImage: "")
+                    .labelStyle(.titleOnly)
+                Slider(value: $point, in: 1...100)
+                Label("100", systemImage: "")
+                    .labelStyle(.titleOnly)
+            }
+            HStack {
+                Button("Hit me") {
+                    print("Button Hit")
+                }
+            }
+            HStack(alignment: .center, spacing: 300) {
+                HStack {
+                    Button("Start Over") {
+                        print("Button hit")
+                    }
+                    Label("Score:", systemImage: "")
+                        .labelStyle(.titleOnly)
+                    Label("999", systemImage: "")
+                        .labelStyle(.titleOnly)
+                }
+                HStack {
+                    Label("Round:", systemImage: "")
+                        .labelStyle(.titleOnly)
+                    Label("999", systemImage: "")
+                        .labelStyle(.titleOnly)
+                    Button(action: {
+                        print("Info button tapped")
+                    }) {
+                        Image(systemName: "info.circle")
+                            .foregroundColor(.blue)
+                    }
+                }
+            }
+            
         }
-        .padding()
     }
 }
 
