76d002f791ba6172fb3c31e751a81ab4c034b9d5
Tina Skordaki <tinaskordaki@gmail.com>
2024-02-24 23:44:59 +0300

    added background image to info View

--- BullsEyeSwiftUI/BullsEyeSwiftUI/ContentView.swift
+++ BullsEyeSwiftUI/BullsEyeSwiftUI/ContentView.swift
@@ -129,6 +129,7 @@ struct ContentView: View {
                         }
                         .buttonStyle(SmallButtonStyle())
                         .sheet(isPresented: $isInfoViewPresented, content: {
+                            // MARK: - TO DO 
                             InfoView()
                         })
                     }
--- BullsEyeSwiftUI/BullsEyeSwiftUI/InfoView.swift
+++ BullsEyeSwiftUI/BullsEyeSwiftUI/InfoView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct InfoView: View {
+    //@Binding var isPresented: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("icBackground")
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity,minHeight: 0, maxHeight: .infinity)
+                .clipped()
+                .ignoresSafeArea(.all)
+            VStack() {
+                if let url = Bundle.main.url(forResource: "bullEye", withExtension: "html") {
+                    WebView(url: url)
+                }
+                Button(action: {
+                    // isPresented = false
+                }) {
+                    Text("Close")
+                }
+                
+            }
+        }
     }
 }
 
-#Preview {
-    InfoView()
-}
+//#Preview {
+//    @State var isPresent = true
+//    InfoView()
+//}
